2481b98131b97f37749052e01a07f8d94efe37ff
Jessie Pearson <223473112+slonepearson@users.noreply.github.com>
2026-05-16 14:17:23 +0200

    added feeds table migration

---
+++ sql/schema/002_feeds.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+CREATE TABLE feeds(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    name TEXT NOT NULL,
+    url TEXT NOT NULL UNIQUE,
+    user_id UUID NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id) 
+        REFERENCES users(id)  
+        ON DELETE CASCADE 
+);
+
+-- +goose Down
+DROP TABLE feeds;
